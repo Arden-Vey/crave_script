1641ce7900aa6a2c35afa373bf7be8db9a1cf8a8
Arden Vey <ardenvey66@gmail.com>
2026-09-13 23:54:31 +0700

    Update lineage_a16.2.sh

--- lineage_a16.2.sh
+++ lineage_a16.2.sh
@@ -3,19 +3,27 @@
 rm -rf .repo/local_manifests/
 
 # Repo init ROM
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth=1
+repo init -u https://github.com/LineageOS/android.git \
+    -b lineage-23.2 \
+    --git-lfs \
+    --depth=1
+
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone -b main https://github.com/JBHPocong/lineage-tissot-manifest.git .repo/local_manifests
+git clone -b main \
+    https://github.com/JBHPocong/lineage-tissot-manifest.git \
+    .repo/local_manifests
+
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
 # Build Sync
 /opt/crave/resync.sh
+
 echo "============="
 echo "Sync success"
 echo "============="
@@ -24,10 +32,12 @@ echo "============="
 export BUILD_USERNAME=Arden-Vey
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
+
 echo "=========================="
 echo "Build environment ready"
 echo "=========================="
@@ -35,10 +45,14 @@ echo "=========================="
 # Lunch
 lunch lineage_tissot_mainline-trunk_staging-userdebug
 
+echo "=================="
+echo "Lunch success"
+echo "=================="
+
 # Build
 mka bacon
 
-# Copy imgs to a separate folder for easy download
+# Copy images
 mkdir -p imgs_output
 
 for img in boot.img system.img vendor.img; do
